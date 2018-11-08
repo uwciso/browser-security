3841aacc63ff2190a54493eb700f6cf79ca63a23
Pete Graff <pgraff@uw.edu>
2018-11-08 11:26:11 -0800

    updating app.js to fully use static assets

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const csp_directives = {
   'default-src': ["*"],
 };
 
+/*****************************************************************************
+ Modify this object with your chosen cookie options 
+*****************************************************************************/
+const cookie_options = {
+
+}
+
 const express = require('express');
 const path = require('path');
 const helmet = require('helmet');
@@ -14,15 +21,19 @@ app.use(helmet.contentSecurityPolicy({
   directives: csp_directives
 }));
 
-app.use(express.static('public'));
+function attach_cookie(url, cookie, value) {
+  return function(req, res, next) {
+    if (req.url === url) {
+      res.cookie(cookie, value, cookie_options);
+    }
+    next();
+  }
+}
 
-app.get('/assets.html', (req, res) => {
-  res.cookie('newcookie', 'chocolate-chip', {
-   /*****************************************************************************
-     Cookie directives go here
-    *****************************************************************************/
-  });
-  res.sendFile(path.join(__dirname + '/assets.html'));
-});
+// set cookies for specific static assets
+app.use(attach_cookie('/assets.html', 'anothertest', 'anothervalue'));
+app.use(attach_cookie('/img/uw-suz.jpg', 'imgtest', 'anothervalue'));
+
+app.use(express.static('public'));
 
 app.listen(3100, () => console.log('App is running at localhost:3100'));
