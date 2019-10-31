101cf3b7f973f09fc8e63c515afb1c56fd8a26d2
Pete Graff <pgraff@uw.edu>
2019-10-31 08:50:16 -0700

    removing comment highlighting

--- public/js/local.js
+++ public/js/local.js
@@ -9,34 +9,16 @@ parent_li.replaceChild(new_span, current_span);
 
 // display source code
 var setToSource = function(id, displayId) {
-	var e = document.getElementById(id);
-        var display = document.getElementById(displayId);
+  var e = document.getElementById(id);
+  var display = document.getElementById(displayId);
 	display.innerText = e.innerHTML;
-        hljs.highlightBlock(display);
-	var xpath = '//text()[contains(., \"//\")]';
-	var xpe = document.evaluate(xpath, display, null, XPathResult.ANY_TYPE, null);
-	var value = xpe.iterateNext();
-	var comments = [];
-	while (value != null) {
-		if (value.textContent.indexOf("\n") == -1 && value.textContent.trim().indexOf('//') == 0) {
-			comments.push(value);
-		}
-		value = xpe.iterateNext();
-	}
-	comments.forEach(function(e) {
-		var wrapper = document.createElement('span');
-                e.parentNode.insertBefore(wrapper, e);
-                wrapper.appendChild(e);
-                wrapper.setAttribute("class", "code-comment");	
-	});
+  hljs.highlightBlock(display);
 };
 
-setTimeout(function() {
-	console.log("document loaded");
+window.onload = function() {
 	setToSource("inlineA", "source-inlineA");
 	setToSource("inlineB", "source-inlineB");
 	setToSource("localScript", "source-localScript");
 	setToSource("localImage", "source-localImage");
 	setToSource("externalImage", "source-externalImage");
-        
-}, 500);
+};
